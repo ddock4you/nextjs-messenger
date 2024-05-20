e6bf794608b0463c44d3998e944d1c568f0c717d
ddock4you <ddock4you@gmail.com>
2024-05-20 22:46:54 +0900

    auth ui 제작 중

---
+++ app/components/AuthForm.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useCallback, useState } from "react";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    }
+  }, [variant]);
+
+  return <div>Auth Form!</div>;
+};
+
+export default AuthForm;

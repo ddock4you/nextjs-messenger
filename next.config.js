dedf5ccaf4efda47c0a079fd76b3d794e5789645
ddock4you <ddock4you@gmail.com>
2024-05-23 23:20:54 +0900

    로그인 ui 제작

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// module.exports = nextConfig;

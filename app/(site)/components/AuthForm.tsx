6d1a767d3080e97219f03c8843eaef86b4c6c7c0
ddock4you <ddock4you@gmail.com>
2024-05-28 22:55:52 +0900

    prisma를 이용하여 mongodb에 데이터 삽입(회원가입) 기능 구현

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
+import axios from "axios";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
@@ -9,7 +10,7 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [variant, setVariant] = useState<Variant>("REGISTER");
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleVariant = useCallback(() => {
@@ -37,6 +38,7 @@ const AuthForm = () => {
 
     if (variant === "REGISTER") {
       // Axios Register
+      axios.post("/api/register", data);
     }
 
     if (variant === "LOGIN") {
@@ -89,28 +91,16 @@ const AuthForm = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
-                Or continue with
-              </span>
+              <span className="bg-white px-2 text-gray-500">Or continue with</span>
             </div>
           </div>
           <div className="mt-6 flex gap-2">
-            <AuthSocialButton
-              icon={BsGithub}
-              onClick={() => socialAction("github")}
-            />
-            <AuthSocialButton
-              icon={BsGoogle}
-              onClick={() => socialAction("google")}
-            />
+            <AuthSocialButton icon={BsGithub} onClick={() => socialAction("github")} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction("google")} />
           </div>
         </div>
         <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
-          <div>
-            {variant === "LOGIN"
-              ? "New to Messenger?"
-              : "Already have an account?"}
-          </div>
+          <div>{variant === "LOGIN" ? "New to Messenger?" : "Already have an account?"}</div>
           <div onClick={toggleVariant} className="underline cursor-pointer">
             {variant === "LOGIN" ? "Create an account" : "Login"}
           </div>
